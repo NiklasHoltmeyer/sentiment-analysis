f96185ea72b391eaaab317d485fa99661c428ccc
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2020-12-02 16:59:00 +0100

    Update 0_install_prerequisites.sh

--- scripts/0_install_prerequisites.sh
+++ scripts/0_install_prerequisites.sh
@@ -2,12 +2,14 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+# Sentiment
 mkdir -p "$SCRIPTPATH/../data/sentiment140"
-wget -nc https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip -P ../data/sentiment140
+wget -nc https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip -P "$SCRIPTPATH/../data/sentiment140"
 unzip -n -d "$SCRIPTPATH/../data/sentiment140/" "$SCRIPTPATH/../data/sentiment140/training.1600000.processed.noemoticon.csv.zip"
 
+# Glove
 mkdir -p "$SCRIPTPATH/../data/glove"
 wget -nc http://nlp.stanford.edu/data/glove.twitter.27B.zip -P "$SCRIPTPATH/../data/glove"
-unzip -n -d "$SCRIPTPATH/../data/glove" "$SCRIPTPATH/../data/glove"/glove.twitter.27B.zip
+unzip -n -d "$SCRIPTPATH/../data/glove" "$SCRIPTPATH/../data/glove/glove.twitter.27B.zip"
 
 python -m pip install -r requirements.txt
