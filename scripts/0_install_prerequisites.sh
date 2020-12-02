8003c5ab1c9326883e79a11f5254bf70add6c9bc
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2020-12-02 16:55:56 +0100

    Update 0_install_prerequisites.sh

--- scripts/0_install_prerequisites.sh
+++ scripts/0_install_prerequisites.sh
@@ -2,18 +2,12 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-pwddir=$(pwd)
-#changing directory just for google colab
-cd "$SCRIPTPATH"
-
-mkdir -p ../data/sentiment140
+mkdir -p "$SCRIPTPATH/../data/sentiment140"
 wget -nc https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip -P ../data/sentiment140
-unzip -n -d ../data/sentiment140/ ../data/sentiment140/training.1600000.processed.noemoticon.csv.zip
+unzip -n -d "$SCRIPTPATH/../data/sentiment140/" "$SCRIPTPATH/../data/sentiment140/training.1600000.processed.noemoticon.csv.zip"
 
-mkdir -p ../data/glove
-wget -nc http://nlp.stanford.edu/data/glove.twitter.27B.zip -P ../data/glove
-unzip -n -d ../data/glove ../data/glove/glove.twitter.27B.zip
+mkdir -p "$SCRIPTPATH/../data/glove"
+wget -nc http://nlp.stanford.edu/data/glove.twitter.27B.zip -P "$SCRIPTPATH/../data/glove"
+unzip -n -d "$SCRIPTPATH/../data/glove" "$SCRIPTPATH/../data/glove"/glove.twitter.27B.zip
 
 python -m pip install -r requirements.txt
-
-cd "$pwddir$
