6ba23b1ff02c98dba139b166fd4ef964044b12a1
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2020-12-21 18:58:31 +0100

    bug fix

--- training/Sentiment140Dataset.py
+++ training/Sentiment140Dataset.py
@@ -24,9 +24,10 @@ class Sentiment140Dataset:
         self.logger.debug('[Sentiment140] Reading Sentiment Dataset')
         startTime = time.time()
         
-        dataset = pd.read_csv(self.path, encoding ="ISO-8859-1" , names=["sentiment", "ids", "date", "flag", "user", "text"],
-                              nrows=CONSTS.TRAINING.NUMBER_OF_TRAINING_DATA_ENTRIES) \
+        dataset = pd.read_csv(self.path, encoding ="ISO-8859-1" , names=["sentiment", "ids", "date", "flag", "user", "text"])
                     .drop(["ids", "date", "flag", "user"], axis=1) 
+                    
+        dataset = dataset.sample(n=CONSTS.TRAINING.NUMBER_OF_TRAINING_DATA_ENTRIES, random_state=42) 
         
         self.logger.debug('[Sentiment140] Reading Sentiment Dataset [DONE] - {} seconds'.format(time.time() - startTime))
         self.logger.debug('[Sentiment140] Clean Sentiment Dataset')
