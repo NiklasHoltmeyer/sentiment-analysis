639db5f5700f45f5f39593a361eb040b6f07f7a7
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2020-12-28 14:18:33 +0100

    Update 0_install_prerequisites_no_glove.sh

--- scripts/0_install_prerequisites_no_glove.sh
+++ scripts/0_install_prerequisites_no_glove.sh
@@ -6,4 +6,4 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 mkdir -p "$SCRIPTPATH/../data/sentiment140"
 wget -nc https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip -P "$SCRIPTPATH/../data/sentiment140"
 unzip -n -d "$SCRIPTPATH/../data/sentiment140/" "$SCRIPTPATH/../data/sentiment140/training.1600000.processed.noemoticon.csv.zip"
-python -m pip install -r "$SCRIPTPATH/requirements.txt"
+python -m pip install -U -r "$SCRIPTPATH/requirements.txt"
