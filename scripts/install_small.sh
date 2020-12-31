66bf55e45e2db0c7d4e9cbf9007ab2fcdc244899
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2020-12-31 16:36:14 +0100

    Update install_small.sh

--- scripts/install_small.sh
+++ scripts/install_small.sh
@@ -18,7 +18,7 @@ unzip -n -d "$SCRIPTPATH/../data/sentiment140/" "$SCRIPTPATH/../data/sentiment14
 #DeepSentiment
 cd "$deepSentimentFolder"
 pip install --user -q -r requirements.txt
-pip install --ignore-installed --progress-bar off -q -e .
+pip install --user --ignore-installed --progress-bar off -q -e .
 
 mkdir -p "$baseDir/training"
 cd "$baseDir/training"
