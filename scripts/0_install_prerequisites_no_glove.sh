fed64554810e82c3c53f2d32ac045823d17ed485
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2020-12-31 14:58:07 +0100

    clean

--- scripts/0_install_prerequisites_no_glove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-# Sentiment
-mkdir -p "$SCRIPTPATH/../data/sentiment140"
-wget -nc https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip -P "$SCRIPTPATH/../data/sentiment140"
-unzip -n -d "$SCRIPTPATH/../data/sentiment140/" "$SCRIPTPATH/../data/sentiment140/training.1600000.processed.noemoticon.csv.zip"
-python -m pip install -U -r "$SCRIPTPATH/requirements.txt"
