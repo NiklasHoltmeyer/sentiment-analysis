52bd7b70d9f4ea027ae3b272fea3af95bc629ba5
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2020-12-31 14:56:11 +0100

    unix style

--- DeepSentiment/setup.py
+++ DeepSentiment/setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup #, find_packages
 
 with open('LICENSE') as f:
     license = f.read()
@@ -16,9 +16,9 @@ setup(
     author='Niklas Holtmeyer',
     url='https://github.com/NiklasHoltmeyer/sentiment-analysis',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=['DeepSentiment', 'DeepSentiment.Consts', 'DeepSentiment.Dataset', 'DeepSentiment.Networks', 'DeepSentiment.Preprocessing', 'DeepSentiment.Networks.Transformer'],#find_packages(exclude=('tests', 'docs')),
     install_requires=[        
-        'tensorflow', 
+        'tensorflow==2.4.0', 
         'pandas', 
         'matplotlib', 
         'nltk', 
@@ -27,6 +27,17 @@ setup(
         'emoji', 
         'transformers', 
         'simpletransformers', 
-        'tqdm'         
+        'tqdm>=4.47.0'         
     ]
+        #'tensorflow==2.4.0',
+        #'pandas', 
+        #'numpy', 
+        #'matplotlib==3.3.3', 
+        #'nltk==3.5', 
+        #'chakin==0.0.8', 
+        #'contractions==0.0.43', 
+        #'emoji==0.6.0', 
+        #'transformers==4.1.1', 
+        #'simpletransformers==0.51.5', 
+        #'tqdm', 
 )
--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
+DeepSentimentFolderName="DeepSentiment"
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 baseDir="$(dirname "$SCRIPTPATH")"
-deepSentimentPath= "$baseDir/DeepSentiment"
+deepSentimentFolder="${baseDir}/DeepSentiment"
+
 # Sentiment
 mkdir -p "$SCRIPTPATH/../data/sentiment140"
 wget -nc https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip -P "$SCRIPTPATH/../data/sentiment140"
@@ -13,9 +15,9 @@ mkdir -p "$SCRIPTPATH/../data/glove"
 wget -nc http://nlp.stanford.edu/data/glove.twitter.27B.zip -P "$SCRIPTPATH/../data/glove"
 unzip -n -d "$SCRIPTPATH/../data/glove" "$SCRIPTPATH/../data/glove/glove.twitter.27B.zip"
 
-cd "$deepSentimentPath"
+#DeepSentiment
+cd "$deepSentimentFolder"
 pip install --progress-bar pretty -q -e .
 
 mkdir -p "$baseDir/training"
 cd "$baseDir/training"
-
