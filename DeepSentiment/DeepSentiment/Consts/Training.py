dc040df7f772901a7fb82cba6348e878b660906d
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2020-12-31 15:01:43 +0100

    Update Training.py

--- DeepSentiment/DeepSentiment/Consts/Training.py
+++ DeepSentiment/DeepSentiment/Consts/Training.py
@@ -3,5 +3,5 @@ EPOCHS = 1
 Learning_Rate = 1e-3
 EARLY_STOPPING_MIN = 1e-3
 EARLY_STOPPING_PATIENCE = 5
-NUMBER_OF_TRAINING_DATA_ENTRIES = 80# <- None = read every entry -> faster testing with less data
+NUMBER_OF_TRAINING_DATA_ENTRIES = None# <- None = read every entry -> faster testing with less data
 BATCH_SIZE = 1024
