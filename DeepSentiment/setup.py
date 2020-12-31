c0b2fbe1ce901569dbdbf31190b73fe7ea386500
Niklas Holtmeyer <niklas.holtmeyer@web.de>
2020-12-31 16:20:50 +0100

    Update setup.py

--- DeepSentiment/setup.py
+++ DeepSentiment/setup.py
@@ -17,27 +17,4 @@ setup(
     url='https://github.com/NiklasHoltmeyer/sentiment-analysis',
     license=license,
     packages=['DeepSentiment', 'DeepSentiment.Consts', 'DeepSentiment.Dataset', 'DeepSentiment.Networks', 'DeepSentiment.Preprocessing', 'DeepSentiment.Networks.Transformer'],#find_packages(exclude=('tests', 'docs')),
-    install_requires=[        
-        'tensorflow==2.4.0', 
-        'pandas', 
-        'matplotlib', 
-        'nltk', 
-        'chakin', 
-        'contractions', 
-        'emoji', 
-        'transformers', 
-        'simpletransformers', 
-        'tqdm>=4.47.0'         
-    ]
-        #'tensorflow==2.4.0',
-        #'pandas', 
-        #'numpy', 
-        #'matplotlib==3.3.3', 
-        #'nltk==3.5', 
-        #'chakin==0.0.8', 
-        #'contractions==0.0.43', 
-        #'emoji==0.6.0', 
-        #'transformers==4.1.1', 
-        #'simpletransformers==0.51.5', 
-        #'tqdm', 
 )
