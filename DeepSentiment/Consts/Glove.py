b11c6c5c03da16427c125ffa9e730917f5fa84fa
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2021-01-07 10:13:44 +0100

    const prefix changed

--- DeepSentiment/Consts/Glove.py
+++ DeepSentiment/Consts/Glove.py
@@ -1,2 +1,3 @@
-GLOVE_FILENAME = "/data/glove/glove.twitter.27B.100d.txt"
+from . import Paths
+GLOVE_FILENAME = Paths.DATA_BASE + "/glove/glove.twitter.27B.100d.txt"
 GLOVE_DIM = 100
--- DeepSentiment/Consts/Paths.py
+++ DeepSentiment/Consts/Paths.py
@@ -2,13 +2,14 @@ import os
 
 BASE = os.getcwd()
 RESULTS_BASE = os.getcwd() + "/results/"
+DATA_BASE = "/data/"
 MODEL_CHECKPOINTS = RESULTS_BASE + "/model_checkpoints/"
 MODEL = RESULTS_BASE + "/model/"
-SENTIMENT140_DATASET = "/data/sentiment140/training.1600000.processed.noemoticon.csv" ##BASE +"\\Datasets\Sentiment140_twitter\\training.1600000.processed.noemoticon.csv"
-SENTIMENT140_DATASET_PARSED = "/data/sentiment140/training_parsed.csv"
-SENTIMENT140_DATASET_PARSED_TSV = ("/data/sentiment140/training_parsed.tsv", "/data/sentiment140/test_parsed.tsv") #lazy_loading only
+SENTIMENT140_DATASET = DATA_BASE +"sentiment140/training.1600000.processed.noemoticon.csv" ##BASE +"\\Datasets\Sentiment140_twitter\\training.1600000.processed.noemoticon.csv"
+SENTIMENT140_DATASET_PARSED = DATA_BASE +"sentiment140/training_parsed.csv"
+SENTIMENT140_DATASET_PARSED_TSV = (DATA_BASE +"sentiment140/training_parsed.tsv", DATA_BASE +"sentiment140/test_parsed.tsv") #lazy_loading only
 SENTIMENT140_URL = "https://nyc3.digitaloceanspaces.com/ml-files-distro/v1/sentiment-analysis-is-bad/data/training.1600000.processed.noemoticon.csv.zip"
     
-IMDB_DATASET = "/data/aclImdb/aclImdb.csv"
-IMDB_DATASET_Parsed = "/data/aclImdb/aclImdb_parsed.csv"
+IMDB_DATASET = DATA_BASE + "aclImdb/aclImdb.csv"
+IMDB_DATASET_Parsed = DATA_BASE + "aclImdb/aclImdb_parsed.csv"
 TRAINING_RESULT_CSV = (RESULTS_BASE, "result.csv") #path, filename
